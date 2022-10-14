c94270b825f86ffb36429af56158507aa78efd84
Nico Valdez <nicomvaldez@gmail.com>
2022-10-14 15:33:23 -0300

    adding demo page

---
+++ demos/pw-vue-app/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Ramp, RampUnit } from '@playwire/pw-vue-component';
+</script>
+
+<template>
+  <header
+    style="display:flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction: column;"
+    >
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Vue App</h1>
+  </header>
+
+  <main>
+    <!-- <Ramp publisherId="1016948" id="63673"> -->
+    <Ramp publisherId="343" id="926">
+      <RampUnit
+        type="leaderboard_atf"
+        selectorId="pwLeaderboardAtf"
+        className="leaderboard"
+      ></RampUnit>
+      <RampUnit
+        type="med_rect_atf"
+        selectorId="pwMedRectAtf"
+        className="med_rect"
+      ></RampUnit>
+    </Ramp>
+  </main>
+</template>
